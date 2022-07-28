475d1fc9f8ef2937fc58afd89b2682d7367b9e90
ItsRahim <rahim1605@gmail.com>
2022-07-28 11:20:54 +0100

    updated redirect to doGet

--- src/main/java/com/example/webapplication/LoginServlet.java
+++ src/main/java/com/example/webapplication/LoginServlet.java
@@ -4,25 +4,27 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet", value = "/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.sendRedirect("searchFlight.jsp");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try (PrintWriter out = response.getWriter()) {
-            String _username = request.getParameter("username");
-            String _password = request.getParameter("password");
-            if(_username.equals("rahim") && _password.equals("password1")) {
-                response.sendRedirect("searchFlight.jsp");
-            }else {
-                response.sendRedirect("index.jsp");
-            }
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        if(username.equals("rahim") && password.equals("password1")) {
+            HttpSession  session = request.getSession(true);
+            session.setAttribute("uName", username);
+            session.setAttribute("uPass", password);
+            doGet(request,response);
+        }else {
+            response.sendRedirect("index.jsp");
+            HttpSession  session = request.getSession(true);
+            session.setAttribute("errorMessage", "Login Failed ");
         }
     }
 }
