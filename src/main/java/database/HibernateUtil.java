e0ca398cf6a941917673c1fb0ae70cb0acb96db2
ItsRahim <rahim1605@gmail.com>
2022-08-01 15:55:49 +0100

    updated session factory and created hibernate config

--- src/main/java/database/HibernateUtil.java
+++ src/main/java/database/HibernateUtil.java
@@ -1,9 +1,36 @@
 package database;
 
+import org.hibernate.query.Query;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateUtil {
 
     public static boolean checkLogin(String email, String pass) {
         //TODO: Implement a login validation
+        try {
+            Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+            configuration.addAnnotatedClass(entity.Users.class);
+            StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties());
+            SessionFactory factory = configuration.buildSessionFactory(builder.build());
+
+            Session session = factory.openSession();
+            Transaction transaction = session.beginTransaction();
+
+            Query query = session.createQuery("FROM users WHERE email_address =: email AND password =: pass");
+            query.setParameter("email", email);
+            query.setParameter("pass", pass);
+
+            transaction.commit();
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 }
