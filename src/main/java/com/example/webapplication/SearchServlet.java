6dbf14ff5c152f7b2a44550fdca37bc529c858e0
ItsRahim <rahim1605@gmail.com>
2022-08-03 17:35:20 +0100

    removed comments

--- src/main/java/com/example/webapplication/SearchServlet.java
+++ src/main/java/com/example/webapplication/SearchServlet.java
@@ -4,9 +4,6 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @WebServlet(name="SearchServlet", urlPatterns = "/search")
 public class SearchServlet extends HttpServlet {
@@ -24,9 +21,7 @@ public class SearchServlet extends HttpServlet {
         String source = request.getParameter("source");
         String destination = request.getParameter("destination");
         String people = request.getParameter("people");
-        System.out.println("working");
-    }
-/*
+
         if(checkFormValidity(date, source, destination, people)) {
             //TODO Query to find flights and output them onto another page
             System.out.println("Valid Form");
@@ -38,6 +33,6 @@ public class SearchServlet extends HttpServlet {
 
     private boolean checkFormValidity(String date, String source, String destination, String people) {
         return date.length() != 0 && source.length() != 0 && destination.length() != 0 && people.length() != 0;
-    }*/
+    }
 
 }
